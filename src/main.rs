88596fec4236b80a234bf4c547097409ec8df6cd
KATO Hayate <dev@hayatek.jp>
2025-03-09 00:31:36 +0900

    feat: Add command options `--help` and `--version`
    Signed-off-by: KATO Hayate <dev@hayatek.jp>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 // SPDX-FileCopyrightText: 2025 KATO Hayate <dev@hayatek.jp>
 // SPDX-License-Identifier: GPL-3.0-only
 
+use bpaf::{Bpaf};
+
+#[derive(Debug, Clone, Bpaf)]
+#[bpaf(options, version)]
+struct Options {
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = options().run();
 }
 
