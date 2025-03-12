7968fc643227c56da6db141e1b599e84a879dff8
KATO Hayate <dev@hayatek.jp>
2025-03-12 22:05:22 +0900

    feat: Implement SMTP server
    Signed-off-by: KATO Hayate <dev@hayatek.jp>

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,34 @@
 // SPDX-FileCopyrightText: 2025 KATO Hayate <dev@hayatek.jp>
 // SPDX-License-Identifier: GPL-3.0-only
 
+mod smtp;
+
+use std::sync::Arc;
+
 use bpaf::{Bpaf};
 use sysexits::ExitCode;
+
+use crate::smtp::SMTPServer;
+
 
 #[derive(Debug, Clone, Bpaf)]
 #[bpaf(options, version)]
 struct Options {
 }
 
-fn main() -> ExitCode {
+
+#[tokio::main]
+async fn main() -> ExitCode {
+    // Parse cmdline arguments
     let args = options().run();
+
+    // Build server
+    let server = Arc::new(SMTPServer::new());
+    // Run server
+    server.run()
+        .await
+        .expect("Failed to start SMTP server");
+
     return ExitCode::Ok;
 }
 
---
+++ src/smtp/server.rs
@@ -0,0 +1,106 @@
+// SPDX-FileCopyrightText: 2025 KATO Hayate <dev@hayatek.jp>
+// SPDX-License-Identifier: GPL-3.0-only
+
+use std::io;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
+
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader, split};
+use tokio::net::{TcpListener, TcpStream};
+
+
+#[derive(Debug)]
+pub struct SMTPServer {
+    is_closed: AtomicBool,
+}
+
+
+impl SMTPServer {
+    pub fn new() -> Self {
+        Self {
+            is_closed: AtomicBool::new(true),
+        }
+    }
+
+    async fn handle_smtp_connection(&self, stream: TcpStream) {
+        let addr = stream.peer_addr().unwrap();
+        let (stream_r, mut stream_w) = split(stream);
+        let mut reader = BufReader::new(stream_r);
+        let mut line = String::new();
+
+        if let Err(e) = stream_w.write_all(b"220 smtp.example.com ESMTP ZephPost\r\n").await {
+            eprintln!("Send error: {}", e);
+        } else {
+
+            loop {
+                // check
+                if self.is_closed.load(Ordering::Acquire) {
+                    if let Err(e) = stream_w.write_all(b"421 smtp.example.com Service not available, closing transmission channel\r\n").await {
+                        eprintln!("Send error: {}", e);
+                    }
+                    break;
+                }
+
+                // read line
+                line.clear();
+                let _bytes = match reader.read_line(&mut line).await {
+                    Ok(0) => break, // connection closed
+                    Ok(n) => n,
+                    Err(e) => {
+                        eprintln!("Receive error: {}", e);
+                        break;
+                    }
+                };
+                println!("Received: {}", line.trim_end());
+
+                // parse SMTP commands
+                if line.trim().split(" ").collect::<Vec<_>>()[0] == "QUIT" {
+                    if let Err(e) = stream_w.write_all(b"221 Bye\r\n").await {
+                        eprintln!("Send error: {}", e);
+                    }
+                    break;
+                } else {
+                    if let Err(e) = stream_w.write_all(b"500 Error: Command not recognized\r\n").await {
+                        eprintln!("Send error: {}", e);
+                        break;
+                    }
+                }
+            }
+        }
+
+        println!("Connection from {} closed", addr);
+    }
+
+    pub async fn run(self: Arc<Self>) -> io::Result<()> {
+        // configuration
+        let port = if !cfg!(debug_assertions) { 25 } else { 2525 };
+
+        // start SMTP server
+        // TODO: IPv6
+        let listener = TcpListener::bind(SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), port)).await?;
+        self.is_closed.store(false, Ordering::Release);
+        println!("Started SMTP server on port {}", port);
+
+        // listen
+        loop {
+            if self.is_closed.load(Ordering::Acquire) {
+                println!("SMTP server closed");
+                break;
+            }
+            let (stream, addr) = listener.accept().await?;
+            println!("Connected from {}", addr);
+            let self_ref = Arc::clone(&self);
+            tokio::spawn(async move {
+                self_ref.handle_smtp_connection(stream).await;
+            });
+        }
+
+        Ok(())
+    }
+
+    pub async fn close(&mut self) {
+        self.is_closed.store(true, Ordering::Release);
+    }
+}
+
