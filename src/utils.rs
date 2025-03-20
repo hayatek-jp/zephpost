e1214489c256142108a478b9bf3513a86deb3780
KATO Hayate <dev@hayatek.jp>
2025-03-20 19:40:51 +0900

    feat: Implement SMTP MAIL command
    Signed-off-by: KATO Hayate <dev@hayatek.jp>

---
+++ src/utils.rs
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: 2025 KATO Hayate <dev@hayatek.jp>
+// SPDX-License-Identifier: GPL-3.0-only
+
+pub fn validate_email_address(addr: &str) -> bool {
+    // TODO: Validate email address
+    true
+}
+
