fe32b095f92d0b4635094e7c2e1f4a8ae3554e76
KATO Hayate <dev@hayatek.jp>
2025-03-21 00:37:38 +0900

    add: Add a structure to store session information
    Signed-off-by: KATO Hayate <dev@hayatek.jp>

--- src/smtp/server.rs
+++ src/smtp/server.rs
@@ -13,6 +13,74 @@ use super::{SmtpCommand, SmtpError};
 
 
 #[derive(Debug)]
+struct SmtpSession {
+    helo_received: bool,
+    sender: Option<String>,
+    recipients: Vec<String>,
+    data: String,
+}
+
+impl SmtpSession {
+    pub fn new() -> Self {
+        Self {
+            helo_received: false,
+            sender: None,
+            recipients: Vec::new(),
+            data: String::new(),
+        }
+    }
+
+    pub fn reset(&mut self) {
+        self.sender = None;
+        self.recipients.clear();
+        self.data.clear();
+    }
+
+    pub fn is_helo_received(&self) -> bool {
+        self.helo_received
+    }
+
+    pub fn set_helo_received(&mut self) {
+        if !self.helo_received {
+            self.helo_received = true;
+        } else {
+            self.reset();
+            self.helo_received = true;
+        }
+    }
+
+    pub fn get_sender(&self) -> &Option<String> {
+        &self.sender
+    }
+
+    pub fn set_sender(&mut self, sender: String) {
+        if self.sender == None {
+            self.sender = Some(sender);
+        } else {
+            self.reset();
+            self.sender = Some(sender);
+        }
+    }
+
+    pub fn get_recipients(&self) -> &Vec<String> {
+        &self.recipients
+    }
+
+    pub fn add_recipients(&mut self, recipient: String) {
+        self.recipients.push(recipient);
+    }
+
+    pub fn get_data(&self) -> &String{
+        &self.data
+    }
+
+    pub fn append_data(&mut self, buf: &str) {
+        self.data.push_str(buf);
+    }
+}
+
+
+#[derive(Debug)]
 pub struct SmtpServer {
     is_closed: AtomicBool,
     hostname: String,
@@ -43,8 +111,8 @@ impl SmtpServer {
         let writer = &mut stream_w;
         let mut line = String::new();
 
-        // states
-        let mut helo = false;
+        // SMTP session
+        let mut session = SmtpSession::new();
 
         if let Ok(_) = self.write_stream(writer, format!("220 {} ESMTP ZephPost\r\n", self.hostname)).await {
             loop {
@@ -68,15 +136,16 @@ impl SmtpServer {
                 // parse SMTP commands
                 match SmtpCommand::parse(&line) {
                     SmtpCommand::HELO(_) => {
-                        helo = true;
+                        session.set_helo_received();
                         if let Err(_) = self.write_stream(writer, format!("250 {}\r\n", self.hostname)).await { break; }
                     },
                     SmtpCommand::EHLO(_) => {
-                        helo = true;
+                        session.set_helo_received();
                         if let Err(_) = self.write_stream(writer, format!("250 {}\r\n", self.hostname)).await { break; }
                     },
-                    SmtpCommand::MAIL(_) => {
-                        if helo {
+                    SmtpCommand::MAIL(mail) => {
+                        if session.is_helo_received() {
+                            session.set_sender(mail.from);
                             if let Err(_) = self.write_stream(writer, "250 OK\r\n").await { break; }
                         } else {
                             let e = SmtpError::BadSequence;
